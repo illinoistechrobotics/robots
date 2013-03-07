a8abf7e93765096b751c8d84bf336d424156b0c8
Peter Chinetti <peter@chinetti.me>
2013-03-07 00:27:23 -0600

    Fixed exceptions

--- src/robots/Fenrir.java
+++ src/robots/Fenrir.java
@@ -30,7 +30,7 @@ import common.Queue;
 import common.Robot;
 
 public class Fenrir extends Robot{
-
+	static int index=0;
 	public Fenrir(Queue q, Communication c){
 		super(q,c);
 	}
@@ -135,7 +135,7 @@ public class Fenrir extends Robot{
 		if(ev.getIndex()==8){
 			GUI.setBar(2,(int)ev.getFValue()*100);
 		}
-		
+		System.out.println(ev.getFValue()+" "+index++);
 	}
 	
 	public void on_imu(Event ev){
